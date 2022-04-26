90c422602d29d812784192e5d95ff7b5fdbd622d
Raghavendra Khare <raghavxk@gmail.com>
2022-04-26 15:07:04 +0530

    feat: added transcript generation functionality.

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from schemas import SummaryResponseSchema
-from utils import if_yt_video_exists
+from utils import if_yt_video_exists, get_subtitles
 
 router = APIRouter(prefix="/api")
 
@@ -13,16 +13,24 @@ async def generate_summary(video_id: str):
 
     # check to ensure if videoid is valid
     resp = if_yt_video_exists(video_id)
-    if resp[1] == False: # yt api is down
+    if resp[1] == False:  # yt api is down
         raise HTTPException(
             status_code=status.HTTP_424_FAILED_DEPENDENCY,
             detail="Could not generate result because YouTube API is not reachable. Try again later.",
         )
 
-    if resp[0] == False: # yt video does not exist
+    if resp[0] == False:  # yt video does not exist
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"No YouTube video exists for requested video id : {video_id}",
         )
+
+    # extracting subtitles of requested video id
+    resp = get_subtitles(video_id=video_id)
+    if resp.get("success") == False: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=resp.get("message")
+        )
+    transcript = resp.get("transcript")
     
     return {"summary": "here will be summary", "videoid": video_id}
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,13 @@
 import requests
 from typing import Tuple
+from youtube_transcript_api import (
+    NoTranscriptAvailable,
+    NoTranscriptFound,
+    TooManyRequests,
+    TranscriptsDisabled,
+    YouTubeTranscriptApi,
+)
+
 from config import settings
 
 # env variables
@@ -21,7 +29,7 @@ def if_yt_video_exists(video_id: str) -> Tuple:
         resp = requests.get(
             f"https://www.googleapis.com/youtube/v3/videos?part=id&id={video_id}&key={YOUTUBE_API_KEY}"
         ).json()
-        if len(resp.get("items"))==0: #video does not exist
+        if len(resp.get("items")) == 0:  # video does not exist
             return False, True
         return True, True
     except Exception as e:
@@ -30,3 +38,60 @@ def if_yt_video_exists(video_id: str) -> Tuple:
         )
         return False, False
 
+
+def get_subtitles(video_id: str):
+    """This function return subtitles for given video_id.
+
+    PARAMETERS:
+        video_id : video id of youtube video
+
+    RETURNS :
+        dict object : {
+            "success": bool,
+            "message": str,
+            "transcript": str,
+        }
+    """
+    try:
+        subtitles = YouTubeTranscriptApi.get_transcript(video_id=video_id)
+        # format reponse object into a paragraph
+        transcript = ""
+        for subtitle in subtitles:
+            transcript += subtitle["text"]
+            transcript += " "
+        return {
+            "success": True,
+            "message": "Subtiles generated successfully.",
+            "transcript": transcript,
+        }
+    except TooManyRequests as e:
+        return {
+            "success": False,
+            "message": "TooManyRequests: YouTube is receiving too many requests from this IP. Wait until the ban on server has been lifted.",
+            "transcript": None,
+        }
+    except TranscriptsDisabled as e:
+        return {
+            "success": False,
+            "message": "Transcripts Disabled : YouTube has disabled subtitles for this video.Summary can not be generated.",
+            "transcript": None,
+        }
+    except NoTranscriptAvailable as e:
+        return {
+            "success": False,
+            "message": "NoTranscripts Available : No Transcripts available for requested video id. Summary can not be generated.",
+            "transcript": None,
+        }
+    except NoTranscriptFound as e:
+        return {
+            "success": False,
+            "message": "NoTranscriptFound : No transcripts were found for requeseted video id. Summary can not be generated.",
+            "transcript": None,
+        }
+    except Exception as e:
+        print("Interval server error : {e} ")
+        return {
+            "success": False,
+            "message": "Internal Server Error : Server failed to generate summary.",
+            "transcript": None,
+        }
